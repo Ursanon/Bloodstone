adcf0458e1f4196b6e27c819abee733e74cacb54
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2020-04-19 18:24:27 +0200

    Added assets copy

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,6 +9,33 @@ int main(int argc, char* argv[])
     
     SDL_Init(SDL_INIT_VIDEO);
 
+    SDL_Window* window = SDL_CreateWindow("Hello!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_Event event;
+    bool isQuitRequested = false;
+
+    while (!isQuitRequested)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                isQuitRequested = true;
+                break;
+            case SDLK_ESCAPE:
+                isQuitRequested = true;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
