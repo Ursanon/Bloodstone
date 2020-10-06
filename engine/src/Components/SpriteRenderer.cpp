3c61b9f375d61171c72ef7f1c8933c9c4a507029
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2020-10-06 20:13:22 +0200

    Added sprite renderer

--- engine/src/Components/SpriteRenderer.cpp
+++ engine/src/Components/SpriteRenderer.cpp
@@ -14,6 +14,31 @@ void bs::SpriteRenderer::Update(const float& deltaTime)
 
 void bs::SpriteRenderer::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+	auto rect = sprite_->getTextureRect();
+	auto transform = entity_->GetTransform();
+	auto position = transform->GetPosition();
+
+	states.texture = sprite_->getTexture();
+
+	sf::VertexArray vertices(sf::TriangleStrip, 4);
+
+	vertices[0].position = sf::Vector2f(position.X, position.Y);
+	vertices[1].position = sf::Vector2f(position.X, position.Y + rect.height);
+	vertices[2].position = sf::Vector2f(position.X + rect.width, position.Y);
+	vertices[3].position = sf::Vector2f(position.X + rect.width, position.Y + rect.height);
+
+	float left = static_cast<float>(rect.left);
+	float right = left + rect.width;
+	float top = static_cast<float>(rect.top);
+	float bottom = top + rect.height;
+
+	vertices[0].texCoords = sf::Vector2f(left, top);
+	vertices[1].texCoords = sf::Vector2f(left, bottom);
+	vertices[2].texCoords = sf::Vector2f(right, top);
+	vertices[3].texCoords = sf::Vector2f(right, bottom);
+
+	target.draw(vertices, states);
+	/*
 	auto transform = entity_->GetTransform();
 	auto pos = transform->GetPosition();
 	sprite_->setPosition(pos.X, pos.Y);
@@ -23,4 +48,5 @@ void bs::SpriteRenderer::draw(sf::RenderTarget& target, sf::RenderStates states)
 	sprite_->setRotation(rot);
 
 	target.draw(*sprite_, states);
+	*/
 }
